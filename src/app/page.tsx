cfa5bb00005340c818eb188ef8d5ddbb7a9b8295
eduardodgdebem <bemeduardode@gmail.com>
2023-11-20 12:10:07 +0100

    vecel trpc test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 
 import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 
 export default async function Home() {
   const session = await getServerAuthSession();
+  const hello = await api.post.hello.query({text: "from server"})
 
   return (
     <section className="h-full overflow-y-auto rounded-lg p-2">
       <div className="flex flex-col items-center justify-center gap-4">
+        {hello.greeting}
         <p className="text-center text-2xl ">
           {session && <span>Logged in as {session.user?.name}</span>}
         </p>
